347718ec171e24c695d27ab40ac818d259fd2af2
pain_kami <luciferdk47@hotmail.com>
2015-03-22 11:35:41 +0800

    set portal

--- aeviou_ios_backend/AppDelegate.m
+++ aeviou_ios_backend/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,6 +17,8 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    ViewController *vc = [[ViewController alloc] init];
+    self.window.rootViewController = vc;
     // Override point for customization after application launch.
     return YES;
 }
